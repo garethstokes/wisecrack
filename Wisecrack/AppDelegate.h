00b319896801adaa6ea097d36330d9d088afd4d7
Gareth Stokes <gareth@betechnology.com.au>
2012-02-09 19:14:52 +1100

    Initial Commit

---
+++ Wisecrack/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Wisecrack
+//
+//  Created by Gareth Stokes on 9/02/12.
+//  Copyright digital five 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Wisecrack/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Wisecrack
+//
+//  Created by Gareth Stokes on 9/02/12.
+//  Copyright digital five 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
